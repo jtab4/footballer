65fcdbf2d4867cbb8c89eb18824260749b3d7a6c
jtab <jakuq12@gmail.com>
2024-07-03 18:21:06 +0200

    implement new backend on NestJs instead of expressjs and add auth & user controllers with login function and prepare to integrate with frontend

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
+
+  @Post('login')
+  async login(@Body() loginDto: { email: string, password: string }) {
+    const user = await this.authService.validateUser(loginDto.email, loginDto.password);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return this.authService.login(user);
+  }
+}
